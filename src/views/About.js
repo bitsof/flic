642e3133f19132226f18748ebb1c2cab37313143
Jeffery Do <timithy4569@gmail.com>
2022-02-20 01:45:43 -0800

    quick modification for about page (#13)

--- src/views/About.js
+++ src/views/About.js
@@ -5,14 +5,14 @@ const About = () => {
   return (
   <div>
     <h1>About</h1>
-    <FakeText />
+    <AboutText />
   </div>
   )
 }
 
-const FakeText = () => (
+const AboutText = () => (
   <p>
-  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    Flic is a sideproject to help gain some dev experience for both Shane and Jeff.
   </p>
   )
 
